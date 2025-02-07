7129293e9c8a807ccc0c6284a579e8e319dc5702
Minidu Thiranjaya <74867152+MiniduTH@users.noreply.github.com>
2025-02-07 23:49:49 +0530

    Initialize backend project with Express, Mongoose, and dotenv; add .gitignore

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const app = express();
+
+//Middleware
+app.use("/",(req,res,next) =>{
+    res.send("It is working")
+})
+
+// MongoDB connection
+mongoose.connect(process.env.DB_URI)
+.then(()=>{
+    console.log("Connected to MongoDB")
+})
+.then(()=>{
+    app.listen(process.env.PORT)
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
+.catch((err=>{
+    console.log(err)
+}))
+
