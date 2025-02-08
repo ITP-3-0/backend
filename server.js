5d4c1350964ed2a5dd04900dcc414f1a00cb4a0a
Nethmi <nethmi.h@olak.org>
2025-02-08 12:23:48 +0530

    link changed

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ require('dotenv').config()
 const app = express();
 
 //Middleware
-app.use("/",(req,res,next) =>{
+app.use("/", (req, res, next) => {
     res.send("It is working")
 })
 
 // MongoDB connection
 mongoose.connect(process.env.DB_URI)
-.then(()=>{
-    console.log("Connected to MongoDB")
-})
-.then(()=>{
-    app.listen(process.env.PORT)
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
-.catch((err=>{
-    console.log(err)
-}))
+    .then(() => {
+        console.log("Connected to MongoDB")
+    })
+    .then(() => {
+        app.listen(process.env.PORT)
+        console.log(`Server is running on  http://localhost:${process.env.PORT}`)
+    })
+    .catch((err => {
+        console.log(err)
+    }))
 
