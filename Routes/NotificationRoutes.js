270d6fb56581616b41988caee8074cbfb62079f3
Minidu Thiranjaya <74867152+MiniduTH@users.noreply.github.com>
2025-03-18 00:22:56 +0530

    feat: enhance notification functionality with user-specific retrieval and read status

--- Routes/NotificationRoutes.js
+++ Routes/NotificationRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-//Insert Controller
+// Insert Controller
 const NotificationController = require("../Controllers/NotificationController");
 
 router.get("/", NotificationController.getAllNotifications);
 router.post("/", NotificationController.addNotification);
+router.get("/:id", NotificationController.getNotificationById); // Route to get a notification by ID
+router.put("/:id", NotificationController.updateNotification); // Route to update a notification
 router.delete("/:id", NotificationController.deleteNotification);
+router.get("/user/notifications", NotificationController.getNotificationsForUser); // Route to get notifications for a specific user
+router.put("/read/:id", NotificationController.markNotificationAsRead); // Route to mark a notification as read
 
-//export
+// Export
 module.exports = router;
